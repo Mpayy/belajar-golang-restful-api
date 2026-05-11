a0512a804fc6068b48064c92570147653e20eb01
AchmadRifaih <rifaih712@gmail.com>
2026-05-11 21:16:14 +0700

    first commit

---
+++ app/database.go
@@ -0,0 +1,18 @@
+package app
+
+import (
+	"belajar-golang-restful-api/helper"
+	"database/sql"
+	"time"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/belajar-golang-restful-api")
+	helper.PanicIfError(err)
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
